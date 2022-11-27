c0f09cdc7b02144be529da517b3da135219bb477
arabboyev01 <95698940+arabboyev01@users.noreply.github.com>
2022-11-27 20:54:29 +0500

    clean: single-pages

--- src/context/createContext.tsx
+++ src/context/createContext.tsx
@@ -1,8 +1,20 @@
 import {createContext} from "react";
 const CreateContextApi = createContext({
-    value: {},
-    services: {},
-    mentor: {},
+    value: {
+        map(element: (item: { name: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal | null | undefined }) => JSX.Element) {
+            return undefined;
+        }
+    },
+    services: {
+        map(element: (item: { name: string | number | boolean | React.ReactFragment | React.ReactPortal | React.ReactElement<any, string | React.JSXElementConstructor<any>> | null | undefined }) => JSX.Element) {
+            return undefined;
+        }
+    },
+    mentor: {
+        map(element: (item: {name: string | number | boolean | React.ReactFragment | React.ReactPortal | React.ReactElement<any, string | React.JSXElementConstructor<any>> | null | undefined}) => JSX.Element) {
+            return undefined;
+        }
+    },
     changeValue: () => {},
     changeServices: () => {},
     changeMentor: () => {},
